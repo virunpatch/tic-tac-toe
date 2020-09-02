174b928400b00344ebea7e664ddc7b5cb9576d2f
virunpatch <guyver125@hotmail.com>
2020-09-02 10:55:52 +0700

    master

---
+++ build/precache-manifest.b39af7096f19b027caf2db66c44cf53c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a7c826bd4c23797e57443dee9f184d2",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "fbc363413d5c8d8b4953",
+    "url": "/tic-tac-toe/static/css/main.acb2ade7.chunk.css"
+  },
+  {
+    "revision": "642f11ce68d236595a80",
+    "url": "/tic-tac-toe/static/js/2.a2c56392.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.a2c56392.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbc363413d5c8d8b4953",
+    "url": "/tic-tac-toe/static/js/main.43c78ccf.chunk.js"
+  },
+  {
+    "revision": "4ad95f08d6b4d56d4069",
+    "url": "/tic-tac-toe/static/js/runtime-main.fa7aabe4.js"
+  }
+]);
